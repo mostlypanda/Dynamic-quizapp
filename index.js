880d1b7aa945212b0eb632ba8cc029508f12fb11
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-15 14:15:52 +0530

    app is live

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const ejs=require('ejs');
+const path=require('path');
+const fs=require('fs');
+
+
+
+const app=express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use("/static", express.static('static'));
+app.set('view engine', 'ejs');
+
+
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`app is live at ${PORT}`);
+});
